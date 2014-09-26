e9d2b42b01ab4483f51f3ff2734ea8dacf8607f5
Octavio Luna <octavio.luna@gmail.com>
2014-09-26 17:31:56 -0500

    Added the ability to create Scala Verticles

---
+++ src/main/scala/com/mycompany/myproject/ScalaPingVerticle.scala
@@ -0,0 +1,21 @@
+package com.mycompany.myproject
+
+import org.vertx.scala.platform.Verticle
+import org.vertx.scala.core.eventbus.Message
+
+/**
+ * ScalaPing Verticle
+ * Octavio Luna
+ *
+ */
+
+class ScalaPingVerticle extends Verticle{
+	override def start(){
+		val log = container.logger
+		vertx.eventBus.registerHandler("ping-address", {message:Message[String] =>
+      		message.reply("pong!")
+      		log info "Sent back pong scala!"
+      	})
+    
+	}
+}
